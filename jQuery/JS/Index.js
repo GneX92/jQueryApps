0978aad8946cf7b302f55a65a92c43a91422e97d
Julian Bliemel <Julian.Bliemel@drheuer.de>
2024-12-02 15:50:52 +0100

    Add project files.

---
+++ jQuery/JS/Index.js
@@ -0,0 +1,24 @@
+﻿
+$("#btnAdd").on("click", () => {
+    if ($("#radioappendselection").val() === "append") {
+        let val = $("#input").val();
+        let option = '<option value="' + val + '">' + val + '</option>';
+        $("#lbEntries").append(option);
+    }
+
+    if ($("#radioappendselection").val() === "prepend") {
+        let val = $("#input").val();
+        let option = '<option value="' + val + '">' + val + '</option>';
+        $("#lbEntries").prepend(option);
+    }
+})
+
+$("#btnRemove").on("click", () => {
+    if ($("#radioappendselection").val() === "append") {
+        $("#lbEntries").last().remove();
+    }
+
+    if ($("#radioappendselection").val() === "prepend") {
+        $("#lbEntries").first().remove();
+    }
+})
